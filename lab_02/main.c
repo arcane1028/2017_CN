169a78884ffe2c1852b61933cffa0787f5bc204a
arcane1028 <arcane1028@gmail.com>
2017-09-18 01:21:16 +0900

    Comment added

--- lab_02/main.c
+++ lab_02/main.c
@@ -39,25 +39,30 @@ int main(int argc, char **argv) {
     int optval;
     int n;
     struct cal_data rdata;
-
+    //실행시 인자수가 맞는지 검사 argv[0]은 실행파일 이름
     if (argc != 2){
         fprintf(stderr, "usage : %s <port>\n",argv[0]);
         exit(1);
     }
-    portno = atoi(argv[1]);
-
+    portno = atoi(argv[1]);  //입력받은 포트번호 저장 atoi는 char* 숫자는 int형으로 전환
+    //소켓 생성 AF_INET은 IPv4, SOCK_DGRAM은 UDP 사용을 의미
     sockfd = socket(AF_INET, SOCK_DGRAM,0);
     if (sockfd<0)
         error("ERROR opening socket");
 
     optval = 1;
+    //소켓 옵션값을 설정하는 함수
+    //SOL_SOCKET은 소켓 레벨에서 처리함을 의미, SO_REUSEADDR 주소를 재사용을 의미
+    //optval  옵션정보를 저장할 버퍼의 위치
     setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, (const void *)&optval, sizeof(int));
 
+    //유닉스 IPv4에 사용되는 주소 구조체 초기화
     bzero((char *) &serveraddr, sizeof(serveraddr));
     serveraddr.sin_family = AF_INET;
     serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
     serveraddr.sin_port = htons((unsigned short)portno);
 
+    //소켓에 포트와 IP주소 지정
     if (bind(sockfd,(struct sockaddr *) &serveraddr, sizeof(serveraddr)) < 0)
         error("ERROR on binding");
     printf("Start to run server!\n");
@@ -66,17 +71,17 @@ int main(int argc, char **argv) {
 
         //bzero(buf, BUFSIZE);
         memset((void *)&rdata, 0x00, sizeof(rdata));
-        n= recvfrom(sockfd, (void *)&rdata, sizeof(rdata), 0, (
-        struct sockaddr *)&clientaddr, &clientlen);
+        //client에서 정보 받음
+        n= recvfrom(sockfd, (void *)&rdata, sizeof(rdata), 0, (struct sockaddr *)&clientaddr, &clientlen);
 
         if (n<0)
             error("ERROR in recvfrom");
-
+        //주소로 host정보 얻음
         hostp = gethostbyaddr((const char *)&clientaddr.sin_addr.s_addr, sizeof(clientaddr.sin_addr.s_addr),AF_INET);
 
         if (hostp == NULL)
             error("ERROR on gethostbyaddr");
-
+        //네트워크 바이트를 char*로 변환
         hostaddrp = inet_ntoa(clientaddr.sin_addr);
 
         if (hostaddrp == NULL)
@@ -85,9 +90,10 @@ int main(int argc, char **argv) {
         printf("Client Info : %s (%d)\n",hostaddrp, clientaddr.sin_port );
         printf("Input : %d %c %d \n", ntohl(rdata.left_num), rdata.op, ntohl(rdata.right_num));
 
+        //네트워크 바이트를 int형으로 변환
         left_num=ntohl(rdata.left_num);
         right_num=ntohl(rdata.right_num);
-
+        //값 계산
         switch (rdata.op)
         {
             case '+':
@@ -109,6 +115,7 @@ int main(int argc, char **argv) {
         }
         printf("Result : %d\n", result );
         rdata.result = htonl(result);
+        // 전송
         n=sendto(sockfd,(void *)&rdata, sizeof(rdata),0, (struct sockaddr *)&clientaddr, clientlen);
 
         if (n<0)
--- lab_02/udpclient.c
+++ lab_02/udpclient.c
@@ -36,54 +36,56 @@ int main(int argc, char **argv) {
     struct cal_data sdata;
     char *hostname;
     char msg[BUFSIZE];
-
+    //실행시 인자수가 맞는지 검사 argv[0]은 실행파일 이름
     if (argc != 3) {
         fprintf(stderr, "usage : %s <hostname>\n", argv[0]);
         exit(0);
     }
 
-    hostname = argv[1];
-    portno = atoi(argv[2]);
+    hostname = argv[1]; //입력받은 서버주소 저장
+    portno = atoi(argv[2]);  //입력받은 포트번호 저장 atoi는 char* 숫자는 int형으로 전환
 
+    //소켓 생성 AF_INET은 IPv4, SOCK_DGRAM은 UDP 사용을 의미
     sockfd = socket(AF_INET, SOCK_DGRAM, 0);
     if(sockfd<0)
-        error("ERROR opening socket");
+        error("ERROR opening socket"); //-1인 경우 에러
 
+    //호스트 정보 구함
     server = gethostbyname(hostname);
     if (server == NULL){
         fprintf(stderr,"ERROR, no such host ad %s\n", hostname);
         exit(0);
     }
-    bzero((char *)&serveraddr, sizeof(serveraddr));
+    //유닉스 IPv4에 사용되는 주소 구조체 초기화
+    bzero((char *)&serveraddr, sizeof(serveraddr));//
     serveraddr.sin_family = AF_INET;
     bcopy((char *)server->h_addr,(char *)&serveraddr.sin_addr.s_addr, server->h_length);
     serveraddr.sin_port = htons(portno);
     while(1) {
         printf("> ");
-        fgets(msg, BUFSIZE, stdin);
+        fgets(msg, BUFSIZE, stdin); //수식 입력
 
-        sscanf(msg, "%d%[^0-9]%d", &left_num, op, &right_num);
+        sscanf(msg, "%d%[^0-9]%d", &left_num, op, &right_num); //입력받은 수식은 변수에 저장
 
         memset((void *) &sdata, 0x00, sizeof(sdata));
-        sdata.left_num = htonl(left_num);
+        sdata.left_num = htonl(left_num); //네트워크 바이트 순서로 변환하여 저장
         sdata.right_num = htonl(right_num);
-        //sdata.left_num = left_num;
-        //sdata.right_num = right_num;
         sdata.op = op[0];
 
         printf(" %d %c %d =", ntohl(sdata.left_num), sdata.op, ntohl(sdata.right_num));
 
         serverlen = sizeof(serveraddr);
+        //server로 전송
         n = sendto(sockfd, (void *) &sdata, sizeof(sdata), 0, &serveraddr, serverlen);
-
+        //-1이면 실패
         if (n < 0)
             error("ERROR in sendto");
-
+        // 서버에서 응답받음
         n = recvfrom(sockfd, (void *) &sdata, sizeof(sdata), 0, &serveraddr, &serverlen);
-
+        // -1이면 실패
         if (n < 0)
             error("ERROR in recvform");
-        printf(" %d\n", ntohl(sdata.result));
+        printf(" %d\n", ntohl(sdata.result));  //결과 출력
 
     }
 
