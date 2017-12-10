4eaaa494b41e7b6846b7f4349b77ad176a7a6010
arcane1028 <arcane1028@gmail.com>
2017-12-10 21:59:10 +0900

    comment add

--- ftp/client.c
+++ ftp/client.c
@@ -21,14 +21,14 @@ struct Cquery
     int command;
     char filename[256];
 };
-
+// 다운로드 받는 함수
 int file_download(int sockfd, char *file)
 {
     struct Cquery query;
     int fd;
     int readn, writen;
     char buf[MAXLINE];
-
+    //저장할 파일 연결, 없으면 생성
     if ((fd = open(file, O_WRONLY | O_CREAT)) == -1)
     {
         printf("download open error\n");
@@ -40,13 +40,15 @@ int file_download(int sockfd, char *file)
     memset(&query, 0x00, sizeof(query));
     query.command = htonl(Q_DOWNLOAD);
     sprintf(query.filename, "%s", file);
-
+    // 서버로 명령문 전송
     if (send(sockfd, (void *) &query, sizeof(query), 0) <= 0)
     {
         return -1;
     }
+    //서버로 데이터 읽음
     while ((readn = recv(sockfd, buf, MAXLINE, 0)) > 0)
     {
+        //파일에 쓰기
         writen = write(fd, buf, readn);
         if (writen != readn)
         {
@@ -58,7 +60,7 @@ int file_download(int sockfd, char *file)
     close(fd);
     return 1;
 }
-
+//파일을 업로드 하는 함수
 int file_upload(int sockfd, char *file)
 {
     struct Cquery query;
@@ -66,7 +68,7 @@ int file_upload(int sockfd, char *file)
     int readn;
     int sendn;
     char buf[MAXLINE];
-
+    //없로드할 파일 연결
     if ((fd = open(file, O_RDONLY|O_CREAT)) == -1)
     {
         printf("upload open error\n");
@@ -76,13 +78,15 @@ int file_upload(int sockfd, char *file)
     memset(&query, 0x00, sizeof(query));
     query.command = htonl(Q_UPLOAD);
     sprintf(query.filename, "%s", file);
-
+    //명령문 전송
     if (send(sockfd, (void *) &query, sizeof(query), 0) <= 0)
     {
         return -1;
     }
+    //서버에서 데이터 읽음
     while ((readn = read(fd, buf, MAXLINE)) > 0)
     {
+        // 파일에 데이터 쓰기
         sendn = send(sockfd, buf, readn, 0);
         if (sendn != readn)
         {
@@ -93,7 +97,7 @@ int file_upload(int sockfd, char *file)
     close(fd);
     return 1;
 }
-
+//서버 파일 리스트 출력
 int file_list(int sockfd)
 {
     struct Cquery query;
@@ -102,7 +106,7 @@ int file_list(int sockfd)
 
     memset(&query, 0x00, sizeof(query));
     query.command = htonl(Q_LIST);
-
+    //명령문 전송
     if (send(sockfd, (void *) &query, sizeof(query), 0) <= 0)
     {
         perror("Send Error\n");
@@ -111,6 +115,7 @@ int file_list(int sockfd)
     memset(buf, 0x00, MAXLINE);
     while (1)
     {
+        //전송 받은 정보 출력
         len = recv(sockfd, buf, MAXLINE, 0);
         if (len <= 0) {
             printf("========================\n");
@@ -121,8 +126,7 @@ int file_list(int sockfd)
     }
     printf("End!\n");
 }
-
-
+// 도움말 출력 함수
 void help(char *progname)
 {
     printf("Usage : %s -h -i [ip] -u [upload filename] -d [download filename] -l\n", progname);
@@ -140,9 +144,10 @@ int main(int argc, char **argv)
     char buf[MAXLINE];
 
     memset(ipaddr, 0x00, sizeof(ipaddr));
-
+    // 실행시 입력받은 옵션을 구한다.
     while ((opt = getopt(argc, argv, "hli:u:d:")) != -1)
     {
+        //옵션에 따라 설정
         switch (opt)
         {
             case 'h':
@@ -167,25 +172,25 @@ int main(int argc, char **argv)
                 return 1;
         }
     }
-
+    //소켓 생성
     if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     {
         printf("socket error\n");
         return 1;
     }
-
+    //소켓에 IP와 포트 지정
     memset((void *) &addr, 0x00, sizeof(addr));
     addr.sin_family = AF_INET;
     addr.sin_addr.s_addr = inet_addr(ipaddr);
     addr.sin_port = htons(PORTNUM);
     clilen = sizeof(addr);
-
+    // 접속요청
     if (connect(sockfd, (struct sockaddr *) &addr, clilen) < 0)
     {
         printf("connect error\n");
         return 0;
     }
-
+    // 해당 함수 실행
     while (1)
     {
         switch (command_type)
--- ftp/server.c
+++ ftp/server.c
@@ -44,26 +44,25 @@ int main(int argc, char **argv)
     pid_t pid;
 
     printf("FTP SERVER START\n");
-
+    //소켓 생성
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd == -1)
     {
         printf("sockfd socket error\n");
         return -1;
     }
-
+    //모든 IP에서 받도록 소켓의 주소 설정
     memset(&addr, 0x00, sizeof(addr));
     addr.sin_family = AF_INET;
     addr.sin_addr.s_addr = htonl(INADDR_ANY);
     addr.sin_port = htons(PORTNUM);
-
+    // 소켓에 IP와 포트번호 지정
     if (bind(sockfd, (struct sockaddr *) &addr, sizeof(addr)) < 0)
     {
         printf("bind error\n");
         return -1;
     }
-
-
+    //클라이언트 접속 요청 기다림
     if (listen(sockfd, 100) != 0)
     {
         printf("listen error\n");
@@ -72,12 +71,16 @@ int main(int argc, char **argv)
     while (1)
     {
         clilen = sizeof(addr);
+        //클라이언트 접속 요청 수락
         cli_sockfd = accept(sockfd, NULL, (socklen_t *) &clilen);
         if (cli_sockfd < 0)
             exit(0);
+        // 프로세스 생성
         pid = fork();
+        // 자식프로세스인 경우
         if (pid == 0)
         {
+            //클라이언트로 온 명령어 처리 하고 종료
             process(cli_sockfd);
             close(cli_sockfd);
             sleep(1);
@@ -91,7 +94,7 @@ int main(int argc, char **argv)
     return 0;
 }
 
-// upload 받
+// upload 받는 함수
 int file_upload(int sockfd, char *filename)
 {
     int fd;
@@ -104,7 +107,7 @@ int file_upload(int sockfd, char *filename)
 
     printf("file_upload\n");
 
-    // send file
+    // 저장할 파일을 연다. 이때 없으면 생성하도록 O_CREAT 옵션을 주었다.
     if ((fd = open(filename, O_WRONLY|O_CREAT)) == -1)
     {
         printf("upload open error\n");
@@ -113,7 +116,7 @@ int file_upload(int sockfd, char *filename)
     }
 
     memset(buf, 0x00, MAXLINE);
-
+    //클라이언트로 온 정보를 파일에 적는다.
     while ((readn = recv(sockfd, buf, MAXLINE, 0)) > 0)
     {
         writen = write(fd, buf, readn);
@@ -123,8 +126,7 @@ int file_upload(int sockfd, char *filename)
         }
         memset(buf, 0x00, MAXLINE);
     }
-    //list add
-
+    //list 파일에 파일정보 입력
     FILE *fp = fopen("list.txt", "w");
     if (fp == NULL)
     {
@@ -145,7 +147,7 @@ int file_upload(int sockfd, char *filename)
     return 1;
 }
 
-//file send
+//file 보내는 함수
 int file_download(int sockfd, char *filename)
 {
     int fd;
@@ -157,7 +159,7 @@ int file_download(int sockfd, char *filename)
 
     printf("file_download\n");
 
-    // send file
+    // 보내고자할 파일을 연다.
     if ((fd = open(filename, O_RDONLY|O_CREAT)) == -1)
     {
         printf("download open error\n");
@@ -165,7 +167,7 @@ int file_download(int sockfd, char *filename)
     }
 
     memset(buf, 0x00, MAXLINE);
-
+    //읽은 데이터를 소켓을 통해 전송한다.
     while ((readn = read(fd, buf, MAXLINE)) > 0)
     {
         sendn = send(sockfd, buf, readn, 0);
@@ -182,7 +184,7 @@ int file_download(int sockfd, char *filename)
 
     return 1;
 }
-
+//리스트 전송하는 함수
 int file_list(int sockfd)
 {
     int sendn;
@@ -190,9 +192,9 @@ int file_list(int sockfd)
     char *filep;
 
     printf("file_list\n");
-
+    //리스트 파일을 연다
     FILE *fp = fopen("list.txt", "r");
-
+    //읽은 내용을 소켓으로 전송한다.
     while (!feof(fp))
     {
         filep=fgets(temp, MAXLINE, fp);
@@ -211,17 +213,19 @@ int file_list(int sockfd)
 
     return 1;
 }
-
+//명령어를 확인하는 함수이다.
 int process(int sockfd)
 {
     struct Cquery query;
 
     while (1)
     {
+        //소켓으로 명령어를 받는다.
         if (recv(sockfd, &query, sizeof(struct Cquery), 0) <= 0)
         {
             return -1;
         }
+        //명령어의 타입에 따라 해당되는 함수를 실행한다.
         query.command = ntohl(query.command);
         switch (query.command)
         {
